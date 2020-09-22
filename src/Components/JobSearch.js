2792d10cb1f950b81e577e20a96b7d5cfd73ae01
shawn <shawnpatel196@gmail.com>
2020-09-22 12:05:12 -0500

    further skeleton setup

---
+++ src/Components/JobSearch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+import axios from 'axios'
+function JobSearch() {
+    return (
+        <div>
+            <Nav/>
+        </div>
+    )
+}
+
+export default JobSearch
